39ec3e16d4007ffa87540bf387fff2ce8964d314
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2024-09-05 19:40:03 +0100

    Empty Operators target

--- Package.swift
+++ Package.swift
@@ -1,24 +1,16 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "FP",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "FP",
-            targets: ["FP"]),
+        .library(name: "FP", targets: ["FP"]),
+        .library(name: "Operators", targets: ["Operators"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "FP"),
-        .testTarget(
-            name: "FPTests",
-            dependencies: ["FP"]
-        ),
+        .target(name: "FP"),
+        .target(name: "Operators", dependencies: ["FP"]),
+        .testTarget(name: "FPTests", dependencies: ["FP"]),
+        .testTarget(name: "OperatorsTests", dependencies: ["Operators"])
     ]
 )
